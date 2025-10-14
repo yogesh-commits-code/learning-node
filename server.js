f0cd1815a767bb40524227ffe85a0feaa00986ed
yogesh-commits-code <yogesh0.dev@gmail.com>
2025-10-14 23:34:36 +0530

    ch-8 get started for routing, writing routes, writing API's, writing chain http methods.

--- server.js
+++ server.js
@@ -45,44 +45,10 @@ app.use((req, res, next) => {
 	console.log(`${req.method} ${req.path}`);
 	next();
 });
-app.get(/\/index(.html)?/, (req, res) => {
-	res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
 
-app.get('/new-page.html', (req, res) => {
-	res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-app.get(
-	'/hello.html',
-	(req, res, next) => {
-		console.log('Attempted to show!');
-		next();
-	},
-	(req, res) => {
-		res.send('Hello, World');
-	}
-);
-
-// chaining route handler
-let one = (req, res, next) => {
-	console.log('One');
-	next();
-};
-
-let two = (req, res, next) => {
-	console.log('two');
-	next();
-};
-
-let three = (req, res) => {
-	console.log('Three');
-	res.send('Finished');
-};
-app.get(/\/chain(.html)?/, [one, two, three]);
-
-app.get('/old-page.html', (req, res) => {
-	res.redirect(301, '/new-page.html'); // 302 by default
-});
+app.use('/', require('./routes/root'));
+app.use('/subDir', require('./routes/subDir'));
+app.use('/employees', require('./routes/api/employees'));
 
 // for all req
 app.all(/\*/, (req, res) => {
