9ccdc1af5669329909fc359ee891612ccb92a5db
yogesh-commits-code <yogesh0.dev@gmail.com>
2025-10-14 19:52:19 +0530

    ch-7 bulid custom middleware such as logEvents, logger, errorHandler. used in built middleware. used third party middleware such as CORS.

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+
+// custom middleware
+app.use(logger);
+
+// cors - Cross Origin Resource Sharing
+const whiteList = [
+	'https://www.yoursite.com',
+	'http://127.0.0.1:5500',
+	'http://localhost:3000',
+];
+const corsOptions = {
+	// checks for the request origin
+	origin: (origin, callback) => {
+		// remove the !origin after development
+		if (whiteList.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('not allowed by CORS'));
+		}
+	},
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // bulit -in middleware to handle urlencoded data
 // in other words, form data:
@@ -57,8 +84,18 @@ app.get('/old-page.html', (req, res) => {
 	res.redirect(301, '/new-page.html'); // 302 by default
 });
 
-app.get(/\*/, (req, res) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// for all req
+app.all(/\*/, (req, res) => {
+	res.status(404);
+	if (req.accepts('html')) {
+		res.sendFile(path.join(__dirname, 'views', '404.html'));
+	} else if (req.accepts('json')) {
+		res.json({ error: '404 page not found ' });
+	} else {
+		res.type('txt').send('404 not found');
+	}
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log('Server is running on PORT : ', PORT));
