be97c76eab2be25d94569cebf997369ace4104a6
yogesh-commits-code <yogesh0.dev@gmail.com>
2025-10-16 22:57:11 +0530

    ch - 9 worte code for emulating the working of different api methods.

--- controllers/employeesControllers.js
+++ controllers/employeesControllers.js
@@ -12,26 +12,72 @@ const getAllEmployees = (req, res) => {
 const createEmployee = (req, res) => {
 	const newEmployee = {
 		id: data.employees[data.employees.length - 1].id + 1 || 1,
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
 	};
+
+	if (!newEmployee.firstname || !newEmployee.lastname) {
+		return res
+			.status(400)
+			.json({ message: 'First and Last names are different.' });
+	}
+
+	data.setEmployees([...data.employees, newEmployee]);
+	res.status(201).json(data.employees);
 };
 
 const updateEmployee = (req, res) => {
-	res.json({
-		firstname: req.body.firstname,
-		lastname: req.body.lastname,
-	});
+	const employee = data.employees.find(
+		(emp) => emp.id === parseInt(req.body.id)
+	);
+	if (!employee) {
+		return res
+			.status(400)
+			.json({ message: `Employee ID ${req.body.id} not found` });
+	}
+
+	if (req.body.firstname) employee.firstname = req.body.firstname;
+	if (req.body.lastname) employee.lastname = req.body.lastname;
+	const filteredArray = data.employees.filter(
+		(emp) => emp.id !== parseInt(req.body.id)
+	);
+	const unsortedArray = [...filteredArray, employee];
+	data.setEmployees(
+		unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+	);
+	res.json(data.employees);
 };
 
 const deleteEmployee = (req, res) => {
-	res.json({
-		id: req.body.id,
-	});
+	const employee = data.employees.find(
+		(emp) => emp.id === parseInt(req.body.id)
+	);
+	if (!employee) {
+		return res
+			.status(400)
+			.json({ message: `Employee ID ${req.body.id} not found` });
+	}
+
+	if (req.body.firstname) employee.firstname = req.body.firstname;
+	if (req.body.lastname) employee.lastname = req.body.lastname;
+	const filteredArray = data.employees.filter(
+		(emp) => emp.id !== parseInt(req.body.id)
+	);
+	data.setEmployees([...filteredArray]);
+	res.json(data.employees);
 };
 
 const getEmployee = (req, res) => {
-	res.json({
-		id: req.params.id,
-	});
+	const employee = data.employees.find(
+		(emp) => emp.id === parseInt(req.params.id)
+	);
+	if (!employee) {
+		return res
+			.status(400)
+			.json({ message: `Employee ID ${req.params.id} not found` });
+	}
+
+	res.json(employee);
 };
 
 module.exports = {
